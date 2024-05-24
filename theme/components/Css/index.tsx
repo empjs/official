ed9c3853df8438fab602828260b3f00cb9016a39
xuhongbin <xuhongbin@yy.com>
2024-05-24 13:37:17 +0800

    feat: 完善tailwind 适配

--- theme/components/Css/index.tsx
+++ theme/components/Css/index.tsx
@@ -1,10 +1,10 @@
 import { ProgressBar } from "../Benchmark/ProgressBar";
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-const ProgressItem = ({ name, max, value }: any) => {
+export const ProgressItem = ({ name, max, value }: any) => {
 	return (
 		<div className="flex flex-center justify-start m-4 flex-col sm:flex-row">
 			<>
-				<p className="mr-2 mb-2 w-20 text-center text-gray-500 dark:text-light-500">
+				<p className="mr-2 mb-2 sm:w-20 w-auto text-center text-gray-500 dark:text-light-500">
 					{name}
 				</p>
 				<ProgressBar value={value} max={max} />
@@ -15,9 +15,10 @@ const ProgressItem = ({ name, max, value }: any) => {
 const SassModern = () => {
 	return (
 		<div className="flex flex-col items-center my-4 z-1">
-			<h2 className="font-bold text-2xl sm:text-4xl">
-				基于 Sass Modern Mode 构建提升 46.7%
+			<h2 className="font-bold text-xl sm:text-4xl md:text-2xl">
+				基于 Modern Mode 构建提升 46.7%
 			</h2>
+			<p>项目总时长提效、sass提升10x以上</p>
 			<ProgressItem name="Modern Mode" value={7.57} max={14.2} />
 			<ProgressItem name="Default Mode" value={14.2} max={14.2} />
 		</div>
